a17ee1625cc54fb7c08309ca11c8881ae6821e55
lkw303 <lkw303@gmail.com>
2021-08-16 11:02:51 +0800

    Revert "hardcoded change of level for tb3"
    This reverts commit 531959a2a95df9ef7614296d3cbe9cf5b4462a7a.

--- src/map_mux.cpp
+++ src/map_mux.cpp
@@ -4,10 +4,8 @@
 #include "nav_msgs/MapMetaData.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "map_mux/MapRequest.h"
-#include "nav_msgs/GetMap.h"
-#include "nav_msgs/LoadMap.h"
 #include "map_server/image_loader.h"
-#include <std_srvs/Empty.h>
+#include "nav_msgs/GetMap.h"
 
 #include "yaml-cpp/yaml.h"
 #include <vector>
@@ -23,48 +21,124 @@ public:
 
     void start(int argc, char** argv)
     { 
-      map_request_sub = node->subscribe("/map_request", 100, &MapMux::map_request_cb, this);
+  
+      //negative so it will not equal to any map number
+      current_map_number = -1;
 
-      map_change_svc = node->serviceClient<nav_msgs::LoadMap>("change_map");
+      std::string default_yaml_file_path = "./example_environment.yaml";
+      
+      if (argc == 2)
+      { 
+        yaml_file_path = std::string(argv[1]);
+        ROS_INFO("input path to yaml file: %s ",yaml_file_path.c_str());
+        yaml_config = YAML::LoadFile(yaml_file_path);
+      }
+      else
+      { 
+        ROS_ERROR("no yaml file path specified.");
+        exit(0);
+      }
+       
+      //add the path of the maps from the yaml file into the map_paths vector
+      for(const auto level : yaml_config)
+      {
+        //TODO: MapRequest to support the use of level_name instead of map number
+        std::string level_name = level.first.as<std::string>();
+        level_names.push_back(level_name);
+        std::cout<<"level name: "<<level_name<<std::endl;
 
+        //path to the yaml file of the map
+
+        std::string package_name = level.second["maps_package"].as<std::string>();
+        
+        std::string yaml_map_path = level.second["maps"]["path"].as<std::string>();
+        yaml_map_path = ros::package::getPath(package_name) + "/" + yaml_map_path;
+        map_paths.push_back(yaml_map_path);
+        std::cout<<"yaml file path to map "<<level_name<<": "<<yaml_map_path<<std::endl;
+
+        //path to the pgm file relative to yaml file
+        std::string pgm_map_path;
+        double origin[3];
+        double resolution;
+        int negate;
+        double occ_th, free_th;
+        MapMode mode = TRINARY;
+
+        YAML::Node yaml_file_node = YAML::LoadFile(yaml_map_path);
+        
+        
+        pgm_map_path = ros::package::getPath(package_name) + "/maps" + "/" + level_name + "/" + yaml_file_node["image"].as<std::string>();
+        std::cout<<"pgm file path to map "<<level_name<<": "<<pgm_map_path<<std::endl;
+
+        std::vector<double> vector_origin = yaml_file_node["origin"].as<std::vector<double>>();
+
+        origin[0] = vector_origin[0];
+        origin[1] = vector_origin[1];
+        origin[2] = vector_origin[2];
+
+        resolution = yaml_file_node["resolution"].as<double>();
+
+        negate = yaml_file_node["negate"].as<int>();
+
+        occ_th = yaml_file_node["occupied_thresh"].as<double>();
+
+        free_th = yaml_file_node["free_thresh"].as<double>();
+
+        std::cout<<"pgm_map_path: "<<pgm_map_path<<std::endl;
+        std::cout<<"origin: "<<origin[0]<<", "<<origin[1]<<", "<<origin[2]<<std::endl;
+        std::cout<<"resolution: "<<resolution<<std::endl;
+        std::cout<<"negate: "<<negate<<std::endl;
+        std::cout<<"occ_th: "<<occ_th<<std::endl;
+        std::cout<<"free_th: "<<free_th<<std::endl;
+
+        nav_msgs::GetMap::Response map_response;
+
+        map_server::loadMapFromFile(&map_response, pgm_map_path.c_str(), resolution, negate, occ_th, free_th, origin, mode);
+        vector_maps.push_back(map_response.map);
+      }
+    
+      map_pub = node->advertise<nav_msgs::OccupancyGrid>("map", 10);
+      map_request_sub = node->subscribe("map_request", 100, &MapMux::map_request_cb, this);
     }
 
     void map_request_cb(const map_mux::MapRequest::ConstPtr& map_request)
     { 
       bool map_exist = false;
 
-      if (map_request->level_name == "MBC_L9")
+      std::string req_level_name = map_request->level_name;
+      if (req_level_name.compare(current_level) == 0)
       {
-        ROS_INFO("Changing to MBC_L9");
+        map_exist = true;
+        std::cout<<"Already publishing desired map of map_number: "<<current_map_number<<std::endl;
+      }
+          
 
-        nav_msgs::LoadMap srv;
-        srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L9.yaml";
-
-
-
-        if (map_change_svc.call(srv))
+      else //publish map
+      { 
+        for(int i = 0; i < level_names.size(); i ++)
         {
-          node->setParam("/tb3_0_free_fleet_client_node/level_name", "L9");
-          ROS_INFO("Response: %s", srv.response.result);
+          if (req_level_name.compare(level_names[i]) == 0)
+          {
+            current_level = req_level_name;
+            std::cout<<"Publishing map of "<<current_level<<std::endl;
+            map_pub.publish(vector_maps[i]);
+            map_exist = true;
+            break;
+          }
         }
       }
-      else if (map_request->level_name == "MBC_L10")
-      {
-        ROS_INFO("Changing to MBC_L10");
+      if (map_exist == false)
+        std::cout<<"map of name: "<<req_level_name<<" does not exist!"<<std::endl;
+    }
 
-        nav_msgs::LoadMap srv;
-        srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L10.yaml";
-
-        if (map_change_svc.call(srv))
-        {
-          node->setParam("/tb3_0_free_fleet_client_node/level_name", "L10");
-          ROS_INFO("Response: %s", srv.response.result);
-        }
-      }
-
-      std_srvs::Empty emptymsg;
-      ros::service::call("/tb3_0/move_base/clear_costmaps",emptymsg);
-
+    void read_map_cb(const nav_msgs::OccupancyGrid::ConstPtr& map_msg)
+    {   
+        nav_msgs::OccupancyGrid map;
+        map.header = map_msg->header;
+        map.info = map_msg->info;
+        map.data = map_msg->data;
+        vector_maps.push_back(map);
+        std::cout<<"size of vector maps is now: "<<vector_maps.size()<<std::endl;
     }
 
     void get_map()
@@ -80,11 +154,12 @@ private:
     
     //subcribers
     ros::Subscriber map_request_sub;
-    ros::ServiceClient map_change_svc;
+    std::vector<ros::Subscriber> vector_map_sub;
 
     //publishers
     ros::Publisher map_pub;
 
+    std::vector<nav_msgs::OccupancyGrid> vector_maps;
     std::vector<std::string> map_paths;
     std::vector<std::string> level_names;
 
