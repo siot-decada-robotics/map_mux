f961407b04a2184b42a6a44e9851334cb8d36aaf
lkw303 <lkw303@gmail.com>
2021-08-16 11:02:31 +0800

    Revert "added config file"
    This reverts commit 89b32a861e08c407c8fdccea17495ef484ed39e8.

--- src/map_mux.cpp
+++ src/map_mux.cpp
@@ -25,72 +25,42 @@ public:
     { 
       map_request_sub = node->subscribe("/map_request", 100, &MapMux::map_request_cb, this);
 
-      map_change_svc = node->serviceClient<nav_msgs::LoadMap>("/tb3_0/change_map");
+      map_change_svc = node->serviceClient<nav_msgs::LoadMap>("change_map");
 
     }
 
     void map_request_cb(const map_mux::MapRequest::ConstPtr& map_request)
     { 
       bool map_exist = false;
-      std::string map_name = "/maps/" + map_request->level_name;
-      bool use_prefix = true;
 
-      if (node->getParam("/map_mux/use_prefix", use_prefix)){
-        std::string map_change_topic = "/" + map_request->robot_name + "/change_map";
-        map_change_svc = node->serviceClient<nav_msgs::LoadMap>(map_change_topic);
-      }
-      else
+      if (map_request->level_name == "MBC_L9")
       {
-        map_change_svc = node->serviceClient<nav_msgs::LoadMap>("/change_map");
-      }
+        ROS_INFO("Changing to MBC_L9");
 
-      std::string map_url = "";
-      if (node->getParam(map_name, map_url)){
-        ROS_INFO("Changing to : %s", map_name.c_str());
         nav_msgs::LoadMap srv;
-        srv.request.map_url = map_url;
+        srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L9.yaml";
+
+
 
         if (map_change_svc.call(srv))
         {
-          node->setParam("/" + map_request->robot_name + "_free_fleet_client_node/level_name", map_request->level_name);
+          node->setParam("/tb3_0_free_fleet_client_node/level_name", "L9");
           ROS_INFO("Response: %s", srv.response.result);
         }
-        else{
-          ROS_INFO("Error at the map_svc Call back, no such service");
+      }
+      else if (map_request->level_name == "MBC_L10")
+      {
+        ROS_INFO("Changing to MBC_L10");
+
+        nav_msgs::LoadMap srv;
+        srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L10.yaml";
+
+        if (map_change_svc.call(srv))
+        {
+          node->setParam("/tb3_0_free_fleet_client_node/level_name", "L10");
+          ROS_INFO("Response: %s", srv.response.result);
         }
       }
-      else{
-        ROS_INFO("No such param for map_name");
-      }
-
-      // if (map_request->level_name == "MBC_L9")
-      // {
-      //   ROS_INFO("Changing to MBC_L9");
-
-      //   nav_msgs::LoadMap srv;
-      //   srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L9.yaml";
-
-
-
-      //   if (map_change_svc.call(srv))
-      //   {
-      //     node->setParam("/tb3_0_free_fleet_client_node/level_name", "L9");
-      //     ROS_INFO("Response: %s", srv.response.result);
-      //   }
-      // }
-      // else if (map_request->level_name == "MBC_L10")
-      // {
-      //   ROS_INFO("Changing to MBC_L10");
-
-      //   nav_msgs::LoadMap srv;
-      //   srv.request.map_url = "/media/abilash/Ubuntu_Data1/ROS/adhoc_ws/src/adhoc_simulations/maps/mbc_L10.yaml";
-
-      //   if (map_change_svc.call(srv))
-      //   {
-      //     node->setParam("/tb3_0_free_fleet_client_node/level_name", "L10");
-      //     ROS_INFO("Response: %s", srv.response.result);
-      //   }
-      // }
 
       std_srvs::Empty emptymsg;
       ros::service::call("/tb3_0/move_base/clear_costmaps",emptymsg);
